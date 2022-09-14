01cf9a701a94329f8fe0ad72febb3df2a881927d
grombnezor <rob.rozenberg2000@gmail.com>
2022-09-14 12:57:53 +0200

    header

--- tappas restaurant/php/includes/header.php
+++ tappas restaurant/php/includes/header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="../css/hoofdmenu.css">
+    <link rel="stylesheet" href="../../css/header.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jim+Nightshade&display=swap" rel="stylesheet">
     <title>headmenu</title>
 </head>
 <body>
