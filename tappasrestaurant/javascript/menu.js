eeb26d2f7b20c244fd7ef5217b5c77305c1a55f0
grombnezor <rob.rozenberg2000@gmail.com>
2022-09-26 17:27:57 +0200

    menufrontend

--- tappasrestaurant/javascript/menu.js
+++ tappasrestaurant/javascript/menu.js
@@ -1,12 +1,32 @@
-const menuItems = document.querySelectorAll(".nav-item")
+const navItems = document.querySelectorAll(".nav-item")
+const foodChange = document.querySelectorAll(".food-amount")
+const foodAmount = document.querySelectorAll(".amount")
+const minFood = document.querySelectorAll(".min-amount")
+const plusfood = document.querySelectorAll(".plus-amount")
 
-
-for (let x = 0; x < menuItems.length; x++) {
-    menuItems[0].classList.add("selected")
-    menuItems[x].addEventListener("click", function () {
-        for (let x = 0; x < menuItems.length; x++) {
-            menuItems[x].classList.remove("selected")
+for (let x = 0; x < navItems.length; x++) {
+    navItems[0].classList.add("selected")
+    navItems[x].addEventListener("click", function () {
+        for (let x = 0; x < navItems.length; x++) {
+            navItems[x].classList.remove("selected")
             this.classList.add("selected")
         }
     })
 }
+
+
+
+
+for(let y= 0; y < foodAmount.length; y++){
+    minFood[y].addEventListener("click", function(){
+            const ammount = this.parentElement.querySelector(".amount"); 
+            if(ammount.textContent == "0"){
+      return
+    }
+    ammount.textContent = ammount.textContent -= 1;
+    })
+    plusfood[y].addEventListener("click", function(){
+        const ammount = this.parentElement.querySelector(".amount");
+        ammount.textContent = ammount.textContent = parseInt(ammount.textContent) + 1;
+      })
+} 
