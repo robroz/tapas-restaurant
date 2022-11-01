3fba97781cacbb51c818d7acdadf1b5edce3b2f0
grombnezor <rob.rozenberg2000@gmail.com>
2022-11-01 13:43:45 +0100

    base menu

--- tappasrestaurant/javascript/menu.js
+++ tappasrestaurant/javascript/menu.js
@@ -1,50 +1,89 @@
 const navItems = document.querySelectorAll(".nav-item")
-const foodChange = document.querySelectorAll(".food-amount")
-const foodAmount = document.querySelectorAll(".amount")
+const foodAmount = document.querySelectorAll(".food-amount")
 const minFood = document.querySelectorAll(".min-amount")
 const plusfood = document.querySelectorAll(".plus-amount")
+const foodList = document.querySelector(".food-list")
+const foodcontainer = document.querySelector(".food-container")
+
+const dishBanner = document.querySelector("#dishes-img")
+const dishesBanners = ["koudetapas", "vlees", "vegetarische", "vis", "desserts", "could-drinks", "warm-drinks", "bier", "wijn", "Cocktails"]
 
 
-
+displayDishes(1)
 //navbar
 for (let x = 0; x < navItems.length; x++) {
     navItems[0].classList.add("selected")
-    navItems[x].addEventListener("click", function () {
+
+   
+
+     navItems[x].addEventListener("click", function () {
+        foodcontainer.scrollTo(0, 0)
+
         for (let x = 0; x < navItems.length; x++) {
             navItems[x].classList.remove("selected")
-            this.classList.add("selected")
         }
+        this.classList.add("selected")
+        let dishesImgsPath = "../fotos/dishesBanner/" + dishesBanners[x] + ".jpg";
+        dishBanner.style.backgroundImage = 'url(' + dishesImgsPath + ')';
+        displayDishes(x + 1)
     })
 }
 
 
-
-//amount dish chooser
-for(let y= 0; y < foodAmount.length; y++){
-    minFood[y].addEventListener("click", function(){
-            const ammount = this.parentElement.querySelector(".amount"); 
-            if(ammount.textContent == "0"){
-      return
-    }
-    ammount.textContent = ammount.textContent -= 1;
-    })
-    plusfood[y].addEventListener("click", function(){
-        const ammount = this.parentElement.querySelector(".amount");
-        ammount.textContent = ammount.textContent = parseInt(ammount.textContent) + 1;
-      })
-} 
-
-
 //dish from database
-function dishRequest(type){
+function displayDishes(type) {
     $.ajax({
-        type: "POST", 
-        url: "../php/menu.php", 
-        data: {
-            type:type
-        },
+        type: "GET",
+        url: `https://localhost:7269/api/Dish/dishtype/${type}`,
         encode: false,
-      }).done(function (data) {
-    
-      });
+    }).done(function (data) {
+        loadDishHTML(data)
+        foodImg(data)
+    });
+}
+
+function foodImg(data) {
+    const dishImg = document.querySelectorAll(".food-img")
+
+    for (let x = 0; x < data.length; x++) {
+        let dish = data[x]
+        dishImg[x].style.backgroundImage = 'url(' + dish.imgPath + ')';
+    }
+
+}
+
+function loadDishHTML(data) {
+    foodList.innerHTML = "";
+    for (let x = 0; x < data.length; x++) {
+        let dish = data[x]
+
+    let foodItem = document.createElement( 'li' );
+    foodItem.className = "food";
+    foodItem.innerHTML = `
+    <li class="food">
+    <span class="food-img"></span>
+    <div class="food-desc">
+    <p class="food-name">${dish.name}</p>
+    <p class="food-ingredients">${dish.description}</p>
+    <div class="food-amount"><span class="min-amount">-</span><p class="amount">0</p><span class="plus-amount">+</span></div>
+    </div>`
+
+    const minBtn = foodItem.querySelector(".min-amount")
+    const plusBtn = foodItem.querySelector(".plus-amount")
+    const ammount = foodItem.querySelector(".amount")
+
+    plusBtn.addEventListener("click", () => {
+        
+        ammount.textContent = ammount.textContent = parseInt(ammount.textContent) + 1;
+    })
+    minBtn.addEventListener("click", () => {
+        if (ammount.textContent == "0") {
+            return
+        }
+        ammount.textContent = ammount.textContent -= 1;
+    })
+
+        foodList.appendChild(foodItem)
+
+    }
 }
